81e5766d95e5ce390d4e8951683eaedac558b715
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-03-19 11:39:22 +0100

    logger logic is done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import {testDBConnection} from './db_services/mysql_conn_setup';
+import logger from './other_services/winstonLogger';
 const app = express();
 
 //Mysql connection test
 testDBConnection(); 
+
+
+//Do this when the server ends 
+process.on('SIGINT', (code) => {
+    logger.end(); 
+    console.log('See ya later silly');
+    process.exit(0);
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
