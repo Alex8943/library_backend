2d559a0cb9b25220ac8b3f22b6362a45f22d6111
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-03-19 11:20:58 +0100

    add winston library

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import {testDBConnection} from './db_services/mysql_conn_setup';
 const app = express();
 
-
+//Mysql connection test
 testDBConnection(); 
 
 app.listen(3000, () => {
