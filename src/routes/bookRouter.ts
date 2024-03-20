670103d34a4df263b49a11c7131bd2c9375b5b77
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-03-20 10:02:17 +0100

    book router completed

--- src/routes/bookRouter.ts
+++ src/routes/bookRouter.ts
@@ -51,5 +51,68 @@ export async function getAllBooks() {
     }
 }
 
+// --------------------- Get range of books ---------------------
+router.get("/books/:range", async (req, res) => {
+    
+    try {
+        const finishRange = Number(req.params.range);
+        const result = await getBooksUpToFinishRange(finishRange);
+        res.status(200).send(result);
+    }
+    catch (error) {
+        logger.error("Error getting books up to finish range: [getBooksUpToFinishRange]", error);
+        res.status(500).send(error);
+    }
+
+});
+
+export async function getBooksUpToFinishRange(finishRange: number) {
+    try {
+        const books = await Books.findAll({
+            limit: finishRange,
+        });
+
+        const bookArray = books.map((book) => book.toJSON());
+        return bookArray;
+    } catch (error) {
+        logger.error("Error getting books up to finish range: [getBooksUpToFinishRange]", error);
+        throw error;
+    }
+}
+
+
+
+// --------------------- Create book ---------------------
+router.post("/book",  async (req, res) => {
+    try{
+        const result = await createBook(req.body);
+        res.status(200).json(result);
+    } catch (error) {
+        logger.error("Error in creating a new book: [createBook, 1]", error);
+        res.status(500).json(error);
+    }
+});
+
+export async function createBook(values: Books) {
+    try{
+        const book = await Books.create({
+            id: values.book_id,
+            title: values.title,
+            picture: values.picture,
+            summary: values.summary,
+            pages: values.pages,
+            amount: values.amount,
+            available_amount: values.available_amount,
+            author_id: values.author_id,
+        },
+        );
+        logger.info("created book: ", book.toJSON());
+        return book.toJSON();
+    }catch(error){
+        logger.error("Error in creating a new book: [createBook, 2]", error);
+        throw error;
+    }
+}
+
 
 export default router;
