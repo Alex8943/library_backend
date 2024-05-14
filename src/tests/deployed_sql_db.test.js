1c6865cbfc21df6edacaf9fb591c79a9d3daf0f0
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-05-14 14:40:14 +0200

    github actions test

--- src/tests/deployed_sql_db.test.js
+++ src/tests/deployed_sql_db.test.js
@@ -3,36 +3,34 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-describe('my local mysql connection test', () => {
+describe('my deployed mysql connection test', () => {
 
     let connection; 
 
     beforeAll(async () => {
         const mysqlConfig = {
-            host: process.env.MYSQL_DEV_DB_HOST, 
-            user: process.env.MYSQL_DEV_DB_USER,
-            password: process.env.MYSQL_DEV_DB_PASSWORD,
-            database: process.env.MYSQL_DEV_DB_NAME, 
-            port: process.env.MYSQL_DEV_DB_PORT
+            host: process.env.MYSQL_PROD_DB_HOST,
+            user: process.env.MYSQL_PROD_DB_USERNAME,
+            password: process.env.MYSQL_PROD_DB_PASSWORD,
+            database: process.env.MYSQL_PROD_DB_NAME,
+            port: process.env.MYSQL_PROD_DB_PORT
         }; 
 
         connection = await mysql.createConnection(mysqlConfig);
-    })
+    });
 
-    test('should connect to local mysql database', async () => {
+    test('should connect to deployed mysql database', async () => {
         try {
             const [rows] = await connection.execute('SELECT DATABASE() AS databaseName');
             const databaseName = rows[0].databaseName;
 
-            expect(databaseName === 'kea-library').tobe(true);
-        }
-        catch (err) {
+            expect(databaseName === 'kea_library').toBe(true);
+        }catch (err) {
             console.error(err);
         }
     });
 
-
     afterAll(async () => {
         await connection.end();
-    })
-})
+    });
+});
