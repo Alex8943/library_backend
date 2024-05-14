56f6fec7f23ed261a10475a26e2931efe5a1a4a6
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-05-14 16:45:09 +0200

    jest.config.js correction

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 // jest.config.js
 exports.default = {
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-    coverageReporters: ["lcov", "text"],
-    moduleFileExtensions: ['js'],
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/{!(app),}.js']
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/{!(app),}.js'],
+  coverageReporters: ["lcov", "text"],
 };
